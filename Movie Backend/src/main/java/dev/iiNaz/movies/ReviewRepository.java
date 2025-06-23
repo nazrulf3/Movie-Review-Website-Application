520a8fd362c84884fd352dbb2a0b19d7b0796442
Nazrul Farid <nazrulf3@gmail.com>
2025-06-23 13:51:27 +0800

    initial commit

---
+++ Movie Backend/src/main/java/dev/iiNaz/movies/ReviewRepository.java
@@ -0,0 +1,9 @@
+package dev.iiNaz.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+ Simple project about movie review website application
